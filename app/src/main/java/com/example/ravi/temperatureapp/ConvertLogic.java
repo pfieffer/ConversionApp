daa8fe020c5ad0bdade293e7703b781c93582811
RaviGarbujaPun <cbpmgr@gmail.com>
2017-06-20 14:58:54 +0545

    TempApp [ ] Add conversion logics

--- app/src/main/java/com/example/ravi/temperatureapp/ConvertLogic.java
+++ app/src/main/java/com/example/ravi/temperatureapp/ConvertLogic.java
@@ -5,13 +5,38 @@ package com.example.ravi.temperatureapp;
  */
 
 public class ConvertLogic {
-    //convert to celsius
-    public static float convertFahrenheitToCelsius(float fahrenheit){
+    //convert fahrenheit to celsius
+    public static Double convertFahrenheitToCelsius(Double fahrenheit){
         return ((fahrenheit - 32)*5 / 9);
     }
+    //convert fah to kelvin
+    public static Double convertFahrenheitToKelvin(Double fahrenheit){
+        return (((fahrenheit - 32) * 5/9) + 273.15 );
+    }
+    //convert fah to
 
-    //converts to fahrenheit
-    public static float convertCelsiusToFahrenheit(float celsius){
+
+    //convert celsius to fahrenheit
+    public static Double convertCelsiusToFahrenheit(Double celsius){
         return ((celsius*9) / 5) +32;
     }
+    //convert celsius to rankine
+    public static  Double convertCelsiusToRankine(Double celsius){
+        return ((celsius*1.8)+491.67);
+    }
+    //convert celsius to Kelvin
+    public static Double convertCelsiusToKelvin(Double celsius){
+        return (celsius + 273.15);
+    }
+
+
+    //convert Kelvin to Celsius
+    public static  Double convertKelvinToCelsius(Double kelvin){
+        return  (kelvin - 273.15);
+    }
+    //kelvin to fahrenheit
+    public static  Double convertKelvinToFahrenheit(Double kelvin){
+        return (((kelvin - 273.15) * 9/5) + 32 );
+    }
+
 }
