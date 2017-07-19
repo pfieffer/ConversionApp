44aba1679221d87036fb711024551f9f3a23ab6e
RaviGarbujaPun <cbpmgr@gmail.com>
2017-07-19 15:13:43 +0545

    ViewPagerUI Add ViewPager UI (working temp conversion)

---
+++ app/src/main/java/com/example/ravi/temperatureapp/LengthFragment.java
@@ -0,0 +1,35 @@
+package com.example.ravi.temperatureapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class LengthFragment extends Fragment {
+
+
+    public LengthFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_length, container, false);
+
+        return view;
+    }
+
+
+}
--- app/src/main/java/com/example/ravi/temperatureapp/MainActivity.java
+++ app/src/main/java/com/example/ravi/temperatureapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.ravi.temperatureapp;
 
 import android.renderscript.Double2;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,90 +15,81 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText editText1,editText2;
-    private Button button;
-    Spinner spinner1, spinner2;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements ViewPagerEx.OnPageChangeListener {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText1 = (EditText) findViewById(R.id.input_value);
-        editText2 = (EditText) findViewById(R.id.output_value);
-        button = (Button) findViewById(R.id.button_calc);
-        spinner1 = (Spinner) findViewById(R.id.spinner1);
-        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        viewPager = (ViewPager) findViewById(R.id.homeViewPager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.homeTabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TemperatureFragment(), "Temperature");
+        adapter.addFragment(new LengthFragment(), "Length");
+//        adapter.addFragment(new FacebookFragment(), "FACEBOOK");
+//        adapter.addFragment(new TwitterFragment(), "TWITTER");
+        viewPager.setAdapter(adapter);
+    }
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editText1.getText().length()==0){
-                    Toast.makeText(MainActivity.this, "Please enter a valid number", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Double inputValue = Double.parseDouble(editText1.getText().toString());
 
-                    if(spinner1.getSelectedItem().toString().equals("Fahrenheit")){
-                        if (spinner2.getSelectedItem().toString().equals("Celsius")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertFahrenheitToCelsius(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Kelvin")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertFahrenheitToKelvin(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Rankine")){
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-                        }
-                        else {
-                            editText2.setText(inputValue.toString());
-                        }
-                    }
-                    else if(spinner1.getSelectedItem().toString().equals("Celsius")){
-                        if (spinner2.getSelectedItem().toString().equals("Fahrenheit")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertCelsiusToFahrenheit(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Kelvin")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertCelsiusToKelvin(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Rankine")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertCelsiusToRankine(inputValue)));
-                        }
-                        else {
-                            editText2.setText(inputValue.toString());
-                        }
-                    }
-                    else if(spinner1.getSelectedItem().toString().equals("Kelvin")){
-                        if (spinner2.getSelectedItem().toString().equals("Celsius")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertKelvinToCelsius(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Fahrenheit")){
-                            editText2.setText(String.valueOf(ConvertLogic.convertKelvinToFahrenheit(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Rankine")){
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
 
-                        }
-                        else {
-                            editText2.setText(inputValue.toString());
-                        }
-                    }
-                    else if(spinner1.getSelectedItem().toString().equals("Rankine")){
-                        if (spinner2.getSelectedItem().toString().equals("Celsius")){
-//                            editText2.setText(String.valueOf(ConvertLogic.convertRankineToCelsius(inputValue)));
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Kelvin")){
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
 
-                        }
-                        else if(spinner2.getSelectedItem().toString().equals("Fahrenheit")){
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
 
-                        }
-                        else {
-                            editText2.setText(inputValue.toString());
-                        }
-                    }
-                }
-            }
-        });
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
